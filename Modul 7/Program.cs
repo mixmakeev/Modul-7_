7d33a1dcb824bd31ae14bebd075b1590f36d8541
aventus <mix79@ya.ru>
2023-03-09 09:10:56 +0300

    Add project files.

---
+++ Modul 7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; // Подключенное пространство имен
+using System.Text;
+
+namespace FirstApplication.ConsoleApp // Объявление пространства имен для данного кода
+{
+    class Program // Объявление класса Program
+    {
+        static void Main(string[] args) // Объявление метода Main
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance); //Решение проблемы с отображением кирилицы 
+            var enc1251 = Encoding.GetEncoding(1251);
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+            System.Console.InputEncoding = enc1251;
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+
+        }
+    }
+}
